a13bc962e1cc32c522f71b01e8e8fda47b105d9b
Chase <hmmchase@gmail.com>
2017-12-18 21:49:14 -0700

    Added HTML and CSS file

--- scripts.js
+++ scripts.js
@@ -1,24 +1,77 @@
 var $inputTitle = $("#input-title");
+var $inputBody = $("#input-body");
 
 $("#button-save").on("click", buttonSave);
 
-setStorage();
+onLoad()
+
+function onLoad() {
+  setStorage();
+  populateDOM();  
+};
+
 
 function setStorage() {
-  if (localStorage.length === 0) {
+  if (localStorage.getItem("ideas") === null) {
     localStorage.setItem("ideas", ("[]"));
   };
   console.log(localStorage);
 };
 
+function populateDOM() {
+  console.log("populate");
+  getIdeas().forEach(function (idea) {
+    makeIdeaCard(idea.id, idea.title, idea.body, idea.quality);
+    console.log(idea);
+  });
+};
+
 function buttonSave(e) {
   e.preventDefault();
-  var newIdea = $inputTitle.val();
+  var newIdea = new Idea(idGenerator(), getTitleInput(), getBodyInput(), 'Swill');
   console.log(newIdea);
-  var existingIdeas = JSON.parse(localStorage.getItem("ideas"));
+  var existingIdeas = getIdeas();
   console.log(existingIdeas);
   existingIdeas.push(newIdea);
   console.log(existingIdeas);
   localStorage.setItem('ideas', JSON.stringify(existingIdeas));
   console.log(localStorage);
+  makeIdeaCard(newIdea.id, newIdea.title, newIdea.body, 'Swill');
+};
+
+function Idea(id, title, body, quality) {
+  this.id = parseInt(id);
+  this.title = title;
+  this.body = body;
+  this.quality = quality;
+};
+
+function getIdeas() {
+  return JSON.parse(localStorage.getItem("ideas"));
+};
+
+function idGenerator() {
+  return Date.now().toString();
+};
+
+function getTitleInput() {
+  var ideaTitle = $inputTitle.val();
+  return ideaTitle;
+};
+
+function getBodyInput() {
+  var inputBody = $inputBody.val();
+  return inputBody;
+};
+
+function makeIdeaCard(id, title, body, quality) {
+  $('#bottom-section').prepend(`
+    <article id="` + id + `" class="idea">
+      <h1 id="idea-title" contenteditable="true">` + title + `</h1>
+      <button id="delete-idea"></button>
+      <p id="idea-text" contenteditable="true">` + body + `</p>
+      <button id="upvote-idea"></button>
+      <button id="downvote-idea"></button>
+      <p id= "quality ` + quality + `"><span>Quality:</span> <span class = "quality-in-DOM">` + quality + `</span> </p>
+    </article>`);
 };
